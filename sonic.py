d0b7789f4b32db52c73ccf68691b2c168cf2d94f
Michael Pineda <michael.pineda@up.ac.pa>
2021-05-02 18:56:01 -0500

    NUEVO CAMBIO

---
+++ sonic.py
@@ -0,0 +1,15 @@
+import retro
+
+env = retro.make('SonicTheHedgehog-Genesis', 'GreenHillZone.Act1')
+
+env.reset()
+
+done = False
+
+while not done:
+    env.render()
+
+    action = env.action_space.sample()
+    print(action)
+    ob, rew, done, info = env.step(action)
+    print("Action ", action, "Reward ", rew)
